d64a99003103ff313ac41721d333122af59f2226
SenolE <senolerdogan@gmail.com>
2026-05-19 22:23:56 -0500

    ENV VAR test DBG#28

--- libs/utils.groovy
+++ libs/utils.groovy
@@ -52,9 +52,9 @@ def deploymentManifest(Map deplCfg){
                 spec:
                     containers:
                     - name: $deplCfg.name
-                        image: $deplCfg.img
-                        ports:
-                        - containerPort: $deplCfg.port
+                    image: $deplCfg.img
+                    ports:
+                    - containerPort: $deplCfg.port
     """.stripIndent()
     echo manifest
 }
