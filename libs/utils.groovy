300616af72174fb6946f28388b9f9bf9c6ac5765
SenolE <senolerdogan@gmail.com>
2026-05-20 01:20:30 -0500

    ENV VAR test DBG#28

--- libs/utils.groovy
+++ libs/utils.groovy
@@ -2,98 +2,96 @@
 
 
 
-// def imageWork(Map imgInfo) {
-//     // Builds and uploads image to Artifactory
+def imageWork(Map imgInfo) {
+    // Builds and uploads image to Artifactory
 
-//     def PORT = sh(script:"grep EXPOSE $imgInfo.srcDir/Dockerfile", returnStdout: true).replace("EXPOSE ","").trim().toInteger()
-//     def IMAGE="$CONTAINER_REGISTRY/$CONTAIER_REPO/$imgInfo.serviceName:$VERSION"
+    def PORT = sh(script:"grep EXPOSE $imgInfo.srcDir/Dockerfile", returnStdout: true).replace("EXPOSE ","").trim().toInteger()
+    def IMAGE="$CONTAINER_REGISTRY/$CONTAIER_REPO/$imgInfo.serviceName:$VERSION"
 
-//     sh """ 
-//             # Solving some image naming problems that ocured at adservice    
-//             export CONTAINERS_SHORT_NAME_ALIASING=on
+    sh """ 
+            # Solving some image naming problems that ocured at adservice    
+            export CONTAINERS_SHORT_NAME_ALIASING=on
 
-//             echo "$imgInfo.serviceName Service Image Creation"       
-//             echo $imgInfo.srcDir
+            echo "$imgInfo.serviceName Service Image Creation"       
+            echo $imgInfo.srcDir
             
-//             # Podman doesn't need BUILDPLATROM, adds itself on the compile time and hates 
-//             # if there is defination in the Dockerfile
+            # Podman doesn't need BUILDPLATROM, adds itself on the compile time and hates 
+            # if there is defination in the Dockerfile
             
-//             # echo "Building $imgInfo.serviceName container"
-//             # sed -i '/ARG BUILDPLATFORM=/d' Dockerfile
-//             # podman build -t $IMAGE .
+            # echo "Building $imgInfo.serviceName container"
+            # sed -i '/ARG BUILDPLATFORM=/d' Dockerfile
+            # podman build -t $IMAGE .
             
-//             # echo "Login to Artifactory"
-//             # podman push --tls-verify=$REGISTRY_USE_TLS $IMAGE
-//         """
-//     // _deploymentManifest([name: imgInfo.serviceName, img: IMAGE, port: PORT]) 
-//     // _serviceManifest([name: imgInfo.serviceName, port: PORT])
+            # echo "Login to Artifactory"
+            # podman push --tls-verify=$REGISTRY_USE_TLS $IMAGE
+        """
+    _deploymentManifest([name: imgInfo.serviceName, img: IMAGE, port: PORT]) 
+    _serviceManifest([name: imgInfo.serviceName, port: PORT])
     
-// }
+}
 
 def createNewManifestBook(){
     // Creates a manifest book and adds namespace for whole deployment
-    echo "HELLO"
-//     sh """
-//         cat <<-'END' >  manifestbook-${env.K8S_NS}.yml
-// ---
-// apiVersion: v1
-// kind: Namespace
-// metadata:
-//     name: ${env.K8S_NS}
-// END
-//     """.stripIndent().trim()
+    sh """ cat <<-'END' >  manifestbook-${env.K8S_NS}.yml
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+    name: ${env.K8S_NS}
+END
+    """.stripIndent().trim()
 }
 
-// def _deploymentManifest(Map deplCfg){
-//     // Expected object for deplCfg [mame:string , img:string , port: integer ] and returns deployment manifest for API
-//     echo "DEPLOYMENT MANIFEST IS CREATING"
-//     sh """ cat << END >> manifestbook-${env.K8S_NS}.yml
-// --- 
-// apiVersion: apps/v1
-// kind: Deployment
-// metadata:
-//     name: $deplCfg.name
-//     labels:
-//         app: $deplCfg.name
-// spec:
-//     replicas: 1
-//     selector:
-//         matchLabels:
-//             app: $deplCfg.name
-//     template:
-//         metadata:
-//             labels:
-//                 app: $deplCfg.name
-//         spec:
-//             containers:
-//             - name: $deplCfg.name
-//                 image: $deplCfg.img
-//                 ports:
-//                 - containerPort: $deplCfg.port
-// END
-//     """.stripIndent()
-// }
+def _deploymentManifest(Map deplCfg){
+    // Expected object for deplCfg [mame:string , img:string , port: integer ] and returns deployment manifest for API
+    echo "DEPLOYMENT MANIFEST IS CREATING"
+    sh """ cat << END >> manifestbook-${env.K8S_NS}.yml
+--- 
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+    name: $deplCfg.name
+    labels:
+        app: $deplCfg.name
+spec:
+    replicas: 1
+    selector:
+        matchLabels:
+            app: $deplCfg.name
+    template:
+        metadata:
+            labels:
+                app: $deplCfg.name
+        spec:
+            containers:
+            - name: $deplCfg.name
+                image: $deplCfg.img
+                ports:
+                - containerPort: $deplCfg.port
+END
+    """.stripIndent()
+}
 
 
-// def _serviceManifest(Map svcCfg){
-//     // Expected object for deplCfg [mame:string , port:integer ] and returns service manifest for API
+def _serviceManifest(Map svcCfg){
+    // Expected object for deplCfg [mame:string , port:integer ] and returns service manifest for API
 
-//     sh """
-//         cat << EOF >> manifestbook-${env.K8S_NS}.yml
-// ---
-// apiVersion: v1
-// kind: Service
-// metadata:
-//     name: $svcCfg.name
-// spec:
-//     selector:
-//         app.kubernetes.io/name: $svcCfg.name
-//     ports:
-//     - protocol: TCP
-//         port: $svcCfg.port
-//         targetPort: $svcCfg.port
-// EOF
-//         """
-// }
+    sh """
+        cat << EOF >> manifestbook-${env.K8S_NS}.yml
+---
+apiVersion: v1
+kind: Service
+metadata:
+    name: $svcCfg.name
+spec:
+    selector:
+        app.kubernetes.io/name: $svcCfg.name
+    ports:
+    - protocol: TCP
+        port: $svcCfg.port
+        targetPort: $svcCfg.port
+EOF
+    """
+}
 
 return this
