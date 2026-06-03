e2bef3dbe07a74fe4d5a42c231d4d3b440fc8b01
SenolE <senolerdogan@gmail.com>
2026-06-02 22:45:27 -0500

    ConfigMap addition #3

--- libs/utils.groovy
+++ libs/utils.groovy
@@ -372,7 +372,7 @@ spec:
 def _createConfigMap(){
 
     sh """
-    echo '
+    echo "
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -380,7 +380,7 @@ metadata:
 data:
     {{- range $key, $value := .Values.commonEnvVars }}
     {{ $key }}: {{ $value | quote }}
-    {{- end }}' >> ${HELM_CHART_PATH}/values.yaml
+    {{- end }}" >> ${HELM_CHART_PATH}/values.yaml
 
     """    
 }
